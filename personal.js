94b980332a70a5c8b564796976b0df937f6d7a8e
HBson4 <101207606+HBson4@users.noreply.github.com>
2023-11-30 22:36:27 -0700

    pls work

--- personal.js
+++ personal.js
@@ -11,5 +11,5 @@ function showSlides() {
     if (slideIndex > slides.length) {slideIndex = 1}  
     
     slides[slideIndex-1].style.display = "flex";
-    setTimeout(showSlides, 5000); // Change image every 4 seconds
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
