28e9c8712538f6bb14d0424c6a53b6477248b46e
HBson4 <101207606+HBson4@users.noreply.github.com>
2023-12-03 23:19:59 -0700

    AWESOME

--- personal.js
+++ personal.js
@@ -1,5 +1,7 @@
 let slideIndex = 0;
+let slideIndex1 = 0;
 showSlides();
+showSlides1();
 
 function showSlides() {
     let i;
@@ -12,4 +14,17 @@ function showSlides() {
     
     slides[slideIndex-1].style.display = "flex";
     setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+function showSlides1() {
+    let x;
+    let slides = document.getElementsByClassName("mySlides1");
+    for (x = 0; x < slides.length; x++) {
+        slides[x].style.display = "none";
+    }
+    slideIndex1++;
+    if (slideIndex1 > slides.length) {slideIndex1 = 1}  
+    
+    slides[slideIndex1-1].style.display = "flex";
+    setTimeout(showSlides1, 5000); // Change image every 5 seconds
 }
