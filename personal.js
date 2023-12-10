4e1ab5afd9916826c3133f6c18a41dd07f804d96
HBson4 <101207606+HBson4@users.noreply.github.com>
2023-12-10 11:49:04 -0700

    remove add button

--- personal.js
+++ personal.js
@@ -44,16 +44,9 @@ function showSlides2() {
     setTimeout(showSlides2, 4250); // Change image every 4.25 seconds
 }
 
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-function myFunction() {
-    // nothing
-}
+// var modal = document.getElementById('id01');
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
